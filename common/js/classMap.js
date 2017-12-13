34d865dfc1489952e70d6ccfa40324677cd869aa
ckk <ckk@163.com>
2017-12-13 13:45:05 +0800

    继续完善头部样式

---
+++ common/js/classMap.js
@@ -0,0 +1,4 @@
+/**
+ * Created by kkt on 2017/7/14.
+ */
+export default ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
--- components/header/header.vue
+++ components/header/header.vue
@@ -13,9 +13,13 @@
          {{seller.description}}/{{seller.deliveryTime}}分钟送达
        </div>
        <div v-if="seller.supports" class="support">
-         <span class="icon"></span>
+         <span class="icon" :class="classMap[seller.supports[0].type]"></span>
          <span class="text">{{seller.supports[0].description}}</span>
        </div>
+     </div>
+     <div class="support-count" v-if="seller.supports">
+       <span class="count">{{seller.supports.length}}个</span>
+       <i class="icon-keyboard_arrow_right"></i>
      </div>
    </div>
    <div class="bulletin-wrapper"></div>
@@ -23,11 +27,12 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import classMap from '../../common/js/classMap';
  export default {
    props: ['seller'],
    data () {
      return {
-
+       classMap: classMap
      };
    },
    mounted () {
@@ -45,10 +50,14 @@
   color: #fff
   background: #000
   .content-wrapper
+    position: relative
     padding: 24px 12px 18px 24px
     font-size: 0
     .avatar
       display: inline-block
+      vertical-align: top
+      img
+        border-radius: 2px
     .content
       display: inline-block
       margin-left: 18px
@@ -69,4 +78,44 @@
           line-height: 18px
           font-weight: bold
       .description
+        margin-bottom: 10px
+        line-height: 12px
+        font-size: 12px
+      .support
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 4px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_1')
+          &.discount
+            bg-image('discount_1')
+          &.guarantee
+            bg-image('guarantee_1')
+          &.invoice
+            bg-image('invoice_1')
+          &.special
+            bg-image('special_1')
+        .text
+          line-height: 10px
+          font-size: 10px
+          color: #fff
+    .support-count
+      position: absolute
+      bottom: 18px
+      right: 12px
+      padding: 0 8px
+      height: 24px
+      line-height: 24px
+      border-radius: 14px
+      background-color: rgba(0,0,0,.2)
+      text-align: center
+      .count
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        font-size: 10px
 </style>
