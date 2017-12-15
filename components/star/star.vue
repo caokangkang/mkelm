94aabc3d2eaa37dc0e6f9ba6806efa12ac670b48
ckk <ckk@163.com>
2017-12-15 17:29:27 +0800

    头部内容完善3

---
+++ components/star/star.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="star" :class="starType">
+     
+ </div>
+</template>
+
+<script type="text/ecmascript-6">
+ export default {
+   data () {
+     return {
+
+     };
+   },
+   components: {
+
+   }
+ };
+</script>
+
+<style scoped lang="stylus">
+
+</style>
