7ccacbd576f6447238407f0824414e7242100362
ckk <ckk@163.com>
2018-01-05 17:58:52 +0800

    完成头部代码，开始商品代码

--- components/goods/goods.vue
+++ components/goods/goods.vue
@@ -1,15 +1,34 @@
 <template>
- <div class="page">
-     这里是goods
+ <div class="goods">
+   <div class="menu-wrapper">
+     <ul>
+       <li v-for="item in goods" class="menu-item"></li>
+     </ul>
+   </div>
  </div>
 </template>
 
 <script type="text/ecmascript-6">
+import axios from 'axios';
+const ERR_OK = 0;
  export default {
+   props: {
+     seller: {
+       type: Object
+     }
+   },
    data () {
      return {
-
+       goods: []
      };
+   },
+   mounted () {
+     axios.get('/api/goods').then(result => {
+       result = result.data;
+       if (result.errno === ERR_OK) {
+         this.goods = result.data;
+       }
+     });
    },
    components: {
 
@@ -19,5 +38,4 @@
 
 <style scoped lang="stylus">
 
- 
 </style>
---
+++ components/icon/icon.vue
@@ -0,0 +1,39 @@
+<template>
+ <div class="page">
+     <span class="icon"></span>
+ </div>
+</template>
+
+<script type="text/ecmascript-6">
+ export default {
+   data () {
+     return {
+
+     };
+   },
+   components: {
+
+   }
+ };
+</script>
+
+<style scoped lang="stylus">
+  .icon
+    display: inline-block
+    vertical-align: top
+    width: 12px
+    height: 12px
+    margin-right: 2px
+    background-size: 12px 12px
+    background-repeat: no-repeat
+    &.decrease
+      bg-image('decrease_3')
+    &.discount
+      bg-image('discount_3')
+    &.guarantee
+      bg-image('guarantee_3')
+    &.invoice
+      bg-image('invoice_3')
+    &.special
+      bg-image('special_3')
+</style>
